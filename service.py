71c4d9a7e30484fff4bdadb5a4aa72c0519410b7
Solol17 <107293343+Solol17@users.noreply.github.com>
2024-11-21 10:03:24 +0500

    Add files via upload
    Добавлен проект телеграм-бота

---
+++ service.py
@@ -0,0 +1,46 @@
+import socket
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+from skit_bot import bot
+
+# Создание класса, который будет представлять собой windows сервис
+class AppServerSvc(win32serviceutil.ServiceFramework):
+    # Имя сервиса, которое будет использоваться для управления сервисом
+    _svc_name_ = "SKIT_bot"
+    # Имя сервиса, которое будет отображаться в диспетчере служб сервиса Windows
+    _svc_display_name_ = "SKIT_application report"
+    # Создание конструктора
+    def __init__(self, args):
+        # Вызов конструктора родительского класса
+        win32serviceutil.ServiceFramework.__init__(self, args)
+        # Создание события, которое будет использоваться для остановки сервиса
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+        # Установка таймаута по умолчанию для сокетов
+        socket.setdefaulttimeout(60)
+
+    # Метод, который вызывается при запуске сервиса
+    def SvcStop(self):
+        # Сообщение о том, что сервс останавливается
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        # Установка события для остановки сервиса
+        win32event.SetEvent(self.hWaitStop)
+
+    # Метод, который вызывается при запуске сервиса
+    def SvcDoRun(self):
+        # Логирование сообщений о запуске сервиса
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
+                              servicemanager.PYS_SERVICE_STARTED,
+                              (self._svc_name_, ''))
+        self.main()
+
+    # Основной метод сервиса, который выполнять основную логику
+    def main(self):
+        # Запуск бота в бесконечном цикле опроса
+        bot.infinity_polling()
+
+if __name__ == '__main__':
+    win32serviceutil.HandleCommandLine(AppServerSvc)
+
+
---
+++ skit_bot.py
@@ -0,0 +1,48 @@
+import logging
+import os
+
+import telebot
+from configurations import geckodriver_path, firefox_location, source_dir, document_path
+from skit.first_project import SkitConnector
+
+# Назначаем токен бота для связи с телеграмом
+bot = telebot.TeleBot(os.getenv("bot_key"))
+# Создание переменных, в которых хранятся данные для авторизации
+current_passwd = os.getenv("current_passwd")
+current_login = os.getenv("current_login")
+
+# Создание экземпляра класса SkitConnector
+skit = SkitConnector(login=current_login, passwd=current_passwd, source=source_dir)
+
+@bot.message_handler(commands=['start', 'help', 'text', 'report'])
+def send_welcome(message):
+    if message.text == "/start":
+        bot.send_message(message.from_user.id, "Доброго времени суток! Начинаю сбор информации по заявкам.")
+
+  #   Если webdriver закрыт, то происходит его открытие + авторизация на сайте
+    if not skit.driver:
+        skit.open_driver(geckodriver_path, firefox_location)
+        skit.get_authorization()
+    # Обращение к методу, создающий словарь из необходимых данных
+    skit.read_source()
+
+    if message.text == "/start":
+        logging.info(f"Получено сообщение /start")
+        yield_answer = skit.get_report()
+        for answer_list in yield_answer:
+            for answer in answer_list:
+                bot.send_message(message.from_user.id, answer, parse_mode="Markdown")
+        bot.send_message(message.from_user.id, "Это, на данный момент, вся актуальная информация по заявкам ЦОП.")
+
+    if message.text == "/report":
+        logging.info(f"Получено сообщение /report")
+        bot.send_message(message.from_user.id, "Доброго времени суток! Начинаю подготовку отчёта.")
+        skit.data_time()
+        skit.formation_table(document_path)
+        bot.send_document(message.chat.id, open(document_path, 'rb'))
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+  bot.reply_to(message, message.text)
+
+bot.infinity_polling()
